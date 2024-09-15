17a545b559f86d59b42654e6d7baed045f3372b3
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-09-15 01:28:21 -0600

    finished adding the nav.tsx code and the candidate.interface.tsx code

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,14 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    readonly name: string | null;
+    readonly login: string | null;
+    readonly location: string | null;
+    readonly avarar_url: string | null;
+    readonly email: string | null;
+    readonly html_url: string | null;
+    readonly repos_url: string | null;
+    readonly bio: string | null;
+    readonly company: string | null;
+}
+
+export default Candidate;
