9bc928bc3f587df6536307fce7835b11355a3487
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-09-15 18:22:50 -0600

    redid candidateSearch.ts and SavedCandidates.tsx

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,14 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 interface Candidate {
     readonly name: string | null;
-    readonly login: string | null;
+    readonly login: string | null; 
     readonly location: string | null;
     readonly avatar_url: string | null;
     readonly email: string | null;
     readonly html_url: string | null;
-    readonly repos_url: string | null;
-    readonly bio: string | null;
     readonly company: string | null;
+    readonly bio: string | null;
 }
 
-export default Candidate;
+export default Candidate
